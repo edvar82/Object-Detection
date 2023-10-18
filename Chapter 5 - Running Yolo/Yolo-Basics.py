36bb383a453692ca3e9842bd4834072e377ea631
Edvar Monteiro <emsn@ic.ufal.br>
2023-10-18 16:13:13 -0300

    :sparkles: feat: using object detection in videos

--- Chapter 5 - Running Yolo/Yolo-Basics.py
+++ Chapter 5 - Running Yolo/Yolo-Basics.py
@@ -3,5 +3,5 @@ import cv2
 
 # Initialize model
 model = YOLO('../Yolo-Weights/yolov8l.pt')
-results = model('Images/2.png', show=True)
+results = model('../Images/1.png', show=True)
 cv2.waitKey(0)

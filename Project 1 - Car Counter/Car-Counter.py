596c269d49693c97d31584af40341ef479ec50c2
Edvar <fodose123@gmail.com>
2023-10-24 16:38:01 -0300

    finish project 1

--- Project 1 - Car Counter/Car-Counter.py
+++ Project 1 - Car Counter/Car-Counter.py
@@ -27,6 +27,9 @@ mask = cv2.imread('mask.png')
 # Tracking
 tracker = Sort(max_age=20, min_hits=3, iou_threshold=0.3)
 
+limits = [400, 297, 673, 297]
+totalCount = []
+
 while True:
     success, img = cap.read()
     imgRegion = cv2.bitwise_and(img, mask)
@@ -60,18 +63,26 @@ while True:
                 detections = np.vstack((detections, currentArray))
 
     resultsTracker = tracker.update(detections)
+    # cv2.line(img, (limits[0], limits[1], limits[2], limits[3]), (0, 0, 255), 5)
+    cv2.line(img, (limits[0], limits[1]), (limits[2], limits[3]), (0, 0, 255), 5)
 
     for result in resultsTracker:
         x1, y1, x2, y2, id = result
-        print(result)
         x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
         w, h = x2 - x1, y2 - y1
-        cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt=2, colorR=(255, 0, 0))
+        cvzone.cornerRect(img, (x1, y1, w, h), l=9, rt=2, colorR=(255, 0, 255))
         cvzone.putTextRect(img, f'{int(id)}', (max(0, x1), max(35, y1)), scale=2, thickness=3
                            , offset=10)
 
-    cv2.imshow("Image", img)
-    cv2.imshow("ImageRegion", imgRegion)
-    cv2.waitKey(0)
+        cx, cy = x1 + w // 2, y1 + h // 2
+        cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED)
 
-#1h e 59m
+        if limits[0] < cx < limits[2] and limits[1] - 15 < cy < limits[1] + 15:
+            if totalCount.count(id) == 0:
+                totalCount.append(id)
+                cv2.line(img, (limits[0], limits[1]), (limits[2], limits[3]), (0, 255, 0), 5)
+
+    cvzone.putTextRect(img, f'Count: {len(totalCount)}', (50, 50))
+    cv2.imshow("Image", img)
+    # cv2.imshow("ImageRegion", imgRegion)
+    cv2.waitKey(1)

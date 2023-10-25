33e707f6840cc86d0edffc6defd7b4bd5b0f1331
Edvar Monteiro <emsn@ic.ufal.br>
2023-10-25 12:58:16 -0300

    Finish project 3

--- Project 3 - PPE Detection/PPEDetection.py
+++ Project 3 - PPE Detection/PPEDetection.py
@@ -6,13 +6,14 @@ import math
 # cap = cv2.VideoCapture(0)  # For Webcam
 # cap.set(3, 640)
 # cap.set(4, 480)
-cap = cv2.VideoCapture('../Videos/ppe-1.mp4')  # For Video
+cap = cv2.VideoCapture('../Videos/ppe-2.mp4')  # For Video
 
 model = YOLO("ppe.pt")
 
 classNames = ['Hardhat', 'Mask', 'NO-Hardhat', 'NO-Mask', 'NO-Safety Vest', 'Person', 'Safety Cone',
               'Safety Vest', 'machinery', 'vehicle']
 
+myColor = (0, 0, 255)
 while True:
     success, img = cap.read()
     results = model(img, stream=True)
@@ -24,14 +25,28 @@ while True:
             x1, y1, x2, y2 = int(x1), int(y1), int(x2), int(y2)
 
             w, h = x2 - x1, y2 - y1
-            cvzone.cornerRect(img, (x1, y1, w, h))
+            # cvzone.cornerRect(img, (x1, y1, w, h))
 
             # Confidence
             conf = math.ceil((box.conf[0] * 100)) / 100
 
             # Class name
             cls = int(box.cls[0])
-            cvzone.putTextRect(img, f'{classNames[cls]} {conf}', (max(0, x1), max(35, y1)), scale=1, thickness=1)
+
+            currentClass = classNames[cls]
+
+            if conf > 0.5:
+                if currentClass == 'Hardhat' or currentClass == 'Mask' or currentClass == 'Safety Vest':
+                    myColor = (0, 255, 0)
+                elif currentClass == 'NO-Hardhat' or currentClass == 'NO-Mask' or currentClass == 'NO-Safety Vest':
+                    myColor = (0, 0, 255)
+                else:
+                    myColor = (255, 0, 0)
+
+                cvzone.putTextRect(img, f'{classNames[cls]} {conf}', (max(0, x1), max(35, y1)), scale=1,
+                                   thickness=1, colorB=myColor, colorT=(255, 255, 255), colorR=myColor, offset=5)
+
+                cv2.rectangle(img, (x1, y1), (x2, y2), myColor, 3)
 
     cv2.imshow("Image", img)
     cv2.waitKey(1)
